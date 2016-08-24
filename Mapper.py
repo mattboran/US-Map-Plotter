5af26b84fa3a59cbbfb785765905ce61ff0402c9
mattboran <mattboran@gmail.com>
2016-08-24 19:10:14 -0400

    Minor changes, considering contour plots, had trouble with numpy.linespace, will work more.

--- Mapper.py
+++ Mapper.py
@@ -21,17 +21,54 @@ def take_action():
 	make_many_api_calls(cities, states, 'longitude', False)
 	make_many_api_calls(cities, states, 'murder rate', False)
 	
+#Assemble data:
+str_y_lon = property_from_cache('longitude')
+str_x_lat = property_from_cache('latitude')
+y_lon = [float(p) for p in str_y_lon]
+x_lat = [float(p) for p in str_x_lat]
+#need to get whatever property we're plotting and normalize it w/ min-max
+query = 'murders'
+str_props = property_from_cache(query)
+properties = [float(p) for p in str_props]
+
+#Here we convert to 'np' format for matplotlib through numpy
+lat_min = min(x_lat)
+lat_max = max(x_lat)
+lon_min = min(y_lon)
+lon_max = max(y_lon)
+prop_min = min(properties)
+prop_max = max(properties)
+
+spatial_resolution = 0.52
+
+x = np.array(x_lat)
+y = np.array(y_lon)
+z = np.array(properties)
+xinum = (lat_max - lat_min)/spatial_resolution	
+yinum = (lon_max - lon_min)/spatial_resolution
+xi = np.linespace(lat_min, lat_max + spatial_resolution, xinum)
+yi = np.linespace(lon_min, lon_max + spatial_resolution, xinum)
+
+xi,yi = np.meshgrid(xi, yi)
+zi = griddata(x, y, z, xi, yi)
+
 map = Basemap(llcrnrlon=-120, llcrnrlat = 20.3, urcrnrlon =-60, urcrnrlat =52.0, 
-	resolution = 'l', projection = 'tmerc', lat_0 = 35.15, lon_0 = -90.5)
+resolution = 'l', projection = 'tmerc', lat_0 = 35.15, lon_0 = -90.5)
 
 map.drawmapboundary(fill_color='aqua')
-map.fillcontinents(color='coral', lake_color='aqua')
 map.drawcoastlines(linewidth=0.25)
 map.drawcountries(linewidth=0.25)
 
-x,y = map( -73.56, 40.39)
+map.fillcontinents(color='coral', lake_color='aqua')
 
-map.plot(x,y, marker='D', color='r')
+map.drawmeridians(np.arange(0,360,15))
+map.drawparallels(np.arange(-90,90,15))
+
+lat, lon = map.makegrid(zi.shape[1], zi.shape[0])
+x,y = map( lat, lon)
+map.contourf(x, y, zi)
+
+#map.plot(x,y, marker='C', color='r')
 
 plt.title('Property plotted on US Map')
 plt.show()
